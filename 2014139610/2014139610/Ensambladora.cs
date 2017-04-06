8c6bf1b0e610310bdead32f5ed3932fe8ee009c3
Clase <Clase>
2017-04-05 20:11:29 -0500

    Avance 5

--- 2014139610/2014139610/Ensambladora.cs
+++ 2014139610/2014139610/Ensambladora.cs
@@ -8,5 +8,18 @@ namespace _2014139610
 {
     public class Ensambladora
     {
+        private TipoAuto _ta;
+        private TipoBus _tb;
+        private TipoCarro _tc;
+        private Carro _carro;
+        public Ensambladora(){
+            _carro = new Carro();
+        }
+        public Ensambladora (TipoAuto tipoAuto, TipoBus tipoBus, TipoCarro tipoCarro){
+            _tc = tipoCarro;
+            _ta = tipoAuto;
+            _tb = tipoBus;
+        }
+        public Carro carro { get { return _carro; } }
     }
 }
