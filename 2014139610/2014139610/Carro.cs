e33fa8c18f8b0d5c8885fe075c2013bc264e96a2
Clase <Clase>
2017-04-05 20:06:53 -0500

    Avance 4

--- 2014139610/2014139610/Carro.cs
+++ 2014139610/2014139610/Carro.cs
@@ -14,20 +14,28 @@ namespace _2014139610
         private int _numAsientos;
         private Propietario _propietario;
         private TipoCarro _tipoCarro;
+        private Llanta _llanta;
+        private Asiento _asiento;
     public string NumSerieMotor { get; set; }
     public string NumSerieChasis { get; set; }
     public Carro (){
-        
+        _llanta = new Llanta();
+        _asiento = new Asiento();
     }
-    public Carro (Volante volante, Parabrisas parabrisas, int numLlantas, int numAsientos,
-            Propietario propietario, TipoCarro tipoCarro, TipoAuto tipoAuto){
+    public Carro(Volante volante, Parabrisas parabrisas, int numLlantas, int numAsientos,
+            Propietario propietario, TipoCarro tipoCarro, TipoAuto tipoAuto)
+    {
         _volante = volante;
         _parabrisas = parabrisas;
         _propietario = propietario;
         _tipoCarro = tipoCarro;
-            _numLlantas = numLlantas;
-            _numAsientos = numAsientos;
-            
+        _numLlantas = numLlantas;
+        _numAsientos = numAsientos;
     }
+        public Volante volante { get {return _volante;}}
+        public Parabrisas parabrisas { get { return _parabrisas; } }
+        public Propietario propietario { get { return _propietario; } }
+        public Asiento asiento { get { return _asiento; } }
+        public Llanta llanta { get { return _llanta; } }
     }
 }
