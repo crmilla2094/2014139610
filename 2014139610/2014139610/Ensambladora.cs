a612f43258b13d95c9ad2ef1a7e5903219e321c2
crmilla2094 <crmilla2094@gmail.com>
2017-04-09 17:06:30 -0500

    Falta Finalizar

--- 2014139610/2014139610/Ensambladora.cs
+++ 2014139610/2014139610/Ensambladora.cs
@@ -13,48 +13,48 @@ namespace _2014139610
         private List<TipoBus> _tb;
         private List<TipoCarro> _tc;
         private List<Carro> _carro;
+        private Carro car;
         private int _numLlantas;
         private int _numAsientos;
         public Ensambladora(){
          _carro = new List<Carro>();
             _ta = new List<TipoAuto>();
+            car = new Carro();
         }
         public void Agregar(Carro carro, string numero) {
             if (numero[0] == 'a')
             {
-                Console.WriteLine("Seleccionar tipo de Auto: \n" +
-                    "[1]Sedan\n"+
-        "[2]PickUp\n"+
-        "[3]Coupe\n" +
-        "[4]HatchBack\n" +
-        "[5]NoDefinido\n");
-                string selec = Console.ReadLine();
-                if (selec == "1")
+                if (numero[1] == 's')
                 {
                     _carro.Add(new Automovil(carro.volante, carro.parabrisas, carro.propietario, TipoCarro.Automovil,
                     TipoAuto.Sedan));
+                    Console.WriteLine("Es un: Sedan");
                 }
                 else {
-                    if (selec == "2")
+                    if (numero[1] == 'p')
                     {
                         _carro.Add(new Automovil(carro.volante, carro.parabrisas, carro.propietario, TipoCarro.Automovil,
                         TipoAuto.PickUp));
+                        Console.WriteLine("Es un: PickUp");
                     }
                     else {
-                        if (selec == "3")
+                        if (numero[1] == 'c')
                         {
                             _carro.Add(new Automovil(carro.volante, carro.parabrisas, carro.propietario, TipoCarro.Automovil,
                             TipoAuto.Coupe));
+                            Console.WriteLine("Es un: Coupe");
                         }
                         else {
-                            if (selec == "4")
+                            if (numero[1] == 'h')
                             {
                                 _carro.Add(new Automovil(carro.volante, carro.parabrisas, carro.propietario, TipoCarro.Automovil,
                                 TipoAuto.HatchBack));
+                                Console.WriteLine("Es un: HatchBack");
                             }
                             else {
 _carro.Add(new Automovil(carro.volante, carro.parabrisas, carro.propietario, TipoCarro.Automovil,
                     TipoAuto.NoDefinido));
+                                Console.WriteLine("Es un: No definido");
                             }
                         }
                     }
@@ -62,24 +62,23 @@ _carro.Add(new Automovil(carro.volante, carro.parabrisas, carro.propietario, Tip
                 
             }
             else {
-                Console.WriteLine("Seleccionar tipo de Bus: \n" +
-                    "[1]Público\n" +
-        "[2]Privado\n" +
-        "[3]NoDefinido\n");
-                string selec = Console.ReadLine();
-                if (selec=="1") {
+                
+                if (numero[1] == '1') {
                     _carro.Add(new bus(carro.volante, carro.parabrisas, carro.propietario, TipoCarro.Bus,
                     TipoBus.Publico));
+                    Console.WriteLine("Es un: Bus Público");
                 }
                 else {
-                    if (selec == "2")
+                    if (numero[1] == '2')
                     {
                         _carro.Add(new bus(carro.volante, carro.parabrisas, carro.propietario, TipoCarro.Bus,
                             TipoBus.Privado));
+                        Console.WriteLine("Es un: Bus privado");
                     }
                     else {
                         _carro.Add(new bus(carro.volante, carro.parabrisas, carro.propietario, TipoCarro.Bus,
                     TipoBus.NoDefinido));
+                        Console.WriteLine("Es un: Bus no definido");
                     }
                 }
             }
@@ -88,15 +87,68 @@ _carro.Add(new Automovil(carro.volante, carro.parabrisas, carro.propietario, Tip
             
                 
        public void Eliminar(Carro carro, string numero) {
+            Carro car = new Carro();
+            Carro carrito = _carro.Find(c => c.NumSerieChasis == numero);
             if (numero[0] == 'a')
             {
-                _carro.Remove(new Automovil(carro.volante, carro.parabrisas, carro.propietario, TipoCarro.Automovil,
-                    TipoAuto.Coupe));
+                if (numero[1] == 's')
+                {
+                    _carro.Remove(new Automovil(carro.volante, carro.parabrisas, carro.propietario, TipoCarro.Automovil,
+                    TipoAuto.Sedan));
+
+                }
+                else
+                {
+                    if (numero[1] == 'p')
+                    {
+                        _carro.Remove(new Automovil(carro.volante, carro.parabrisas, carro.propietario, TipoCarro.Automovil,
+                        TipoAuto.PickUp));
+                    }
+                    else
+                    {
+                        if (numero[1] == 'c')
+                        {
+                            _carro.Remove(new Automovil(carro.volante, carro.parabrisas, carro.propietario, TipoCarro.Automovil,
+                            TipoAuto.Coupe));
+                        }
+                        else
+                        {
+                            if (numero[1] == 'h')
+                            {
+                                _carro.Remove(new Automovil(carro.volante, carro.parabrisas, carro.propietario, TipoCarro.Automovil,
+                                TipoAuto.HatchBack));
+                            }
+                            else
+                            {
+                                _carro.Remove(new Automovil(carro.volante, carro.parabrisas, carro.propietario, TipoCarro.Automovil,
+                                                    TipoAuto.NoDefinido));
+                            }
+                        }
+                    }
+                }
+
             }
             else
             {
-                _carro.Remove(new bus(carro.volante, carro.parabrisas, carro.propietario, TipoCarro.Bus,
-                    TipoBus.Privado));
+
+                if (numero[1] == '1')
+                {
+                    _carro.Remove(new bus(carro.volante, carro.parabrisas, carro.propietario, TipoCarro.Bus,
+                    TipoBus.Publico));
+                }
+                else
+                {
+                    if (numero[1] == '2')
+                    {
+                        _carro.Remove(new bus(carro.volante, carro.parabrisas, carro.propietario, TipoCarro.Bus,
+                            TipoBus.Privado));
+                    }
+                    else
+                    {
+                        _carro.Remove(new bus(carro.volante, carro.parabrisas, carro.propietario, TipoCarro.Bus,
+                    TipoBus.NoDefinido));
+                    }
+                }
             }
         }
         public void IniciarPersonalizacion(string numero) {
--- 2014139610/2014139610/Program.cs
+++ 2014139610/2014139610/Program.cs
@@ -13,25 +13,10 @@ namespace _2014139610
             Volante vol1 = new Volante("avol001");
             Parabrisas par1 = new Parabrisas("apar001");
             Propietario pers1 = new Propietario("70874455", "Roberto", "Aranguren", "ABC120");
-            numChasis = "achasis01";
+            numChasis = "aschasis01";
 
             var a = new Ensambladora();
             var c = new Carro();
-
-            /*Console.WriteLine("Volante: ");
-            string vol = Console.ReadLine();
-            Volante vol1 = new Volante(vol);
-            Console.WriteLine("Parabrisas: ");
-            string par = Console.ReadLine();
-            Parabrisas par1 = new Parabrisas(par);
-            Console.WriteLine("Nombre: ");
-            string nom = Console.ReadLine();
-            Console.WriteLine("Apellido: ");
-            string ape = Console.ReadLine();
-            Console.WriteLine("Licencia: ");
-            string lic = Console.ReadLine();
-            Console.WriteLine("Dni: ");
-            string dni = Console.ReadLine();*/
 
             Carro carro1 = new Carro(vol1, par1, pers1, TipoCarro.Automovil);
 
@@ -39,7 +24,7 @@ namespace _2014139610
             
 
             Volante vol2 = new Volante("avol002");
-            numChasis = "achasis02";
+            numChasis = "apchasis02";
             Parabrisas par2 = new Parabrisas("apar002");
             Propietario pers2 = new Propietario("70874480", "Roberto", "Milla", "ABC121");
             Carro carro2 = new Carro(vol2, par2, pers2, TipoCarro.Automovil);
@@ -49,7 +34,7 @@ namespace _2014139610
 
             Volante vol3 = new Volante("avol003");
             Parabrisas par3 = new Parabrisas("apar003");
-            numChasis = "achasis03";
+            numChasis = "acchasis03";
             Propietario pers3 = new Propietario("06692345", "Johny", "Cuya", "ABC122");
             Carro carro3 = new Carro(vol1, par1, pers1, TipoCarro.Automovil);
             a.IniciarPersonalizacion(numChasis);
@@ -60,7 +45,7 @@ namespace _2014139610
             Parabrisas par4 = new Parabrisas("apar004");
             Propietario pers4 = new Propietario("70874460", "Consuelo", "Egoavil", "ABC123");
             Carro carro4 = new Carro(vol4, par4, pers4, TipoCarro.Automovil);
-            numChasis = "achasis04";
+            numChasis = "ahchasis04";
             a.IniciarPersonalizacion(numChasis);
             a.Agregar(carro4, numChasis);
             
@@ -69,7 +54,7 @@ namespace _2014139610
             Parabrisas par5 = new Parabrisas("bpar005");
             Propietario pers5 = new Propietario("06752810", "Deissy", "Cuya", "ABC124");
             Carro carro5 = new Carro(vol5, par5, pers5, TipoCarro.Bus);
-            numChasis = "bchasis05";
+            numChasis = "b1chasis05";
             a.IniciarPersonalizacion(numChasis);
             a.Agregar(carro5,numChasis);
             
@@ -78,7 +63,7 @@ namespace _2014139610
             Parabrisas par6 = new Parabrisas("bpar006");
             Propietario pers6 = new Propietario("06795134", "Rosario", "Milla", "ABC125");
             Carro carro6 = new Carro(vol5, par5, pers5, TipoCarro.Bus);
-            numChasis = "bchasis06";
+            numChasis = "b2chasis06";
             a.IniciarPersonalizacion(numChasis);
             a.Agregar(carro5,numChasis);
             
